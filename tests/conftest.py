754077d6d0e74d860932a603ffaa881311f6db18
dyxn.coffeetonight <dyxn.coffeetonight@gmail.com>
2026-06-25 07:00:13 +0000

    .

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,12 @@ from __future__ import annotations
 
 import pytest
 
+from hierwalk.manifest import clear_digest_scope
 from hierwalk.path_walk import clear_path_walk_suite_session
 
 
 @pytest.fixture(autouse=True)
-def _reset_path_walk_suite_session():
+def _reset_path_walk_global_state():
     yield
     clear_path_walk_suite_session()
+    clear_digest_scope()
